d0a365ed6d0a63c57735df88dcafe4e655b26e5d
Wockaflocka23 <140996587+Wockaflocka23@users.noreply.github.com>
2025-06-15 11:05:09 +0530

    Add files via upload

---
+++ server2.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the Home Page!');
+});
+
+
+app.get('/about', (req, res) => {
+  res.send('This is the About Page.');
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
